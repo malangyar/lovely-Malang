7d11baca7b719459a40d3c35f3114e863e41d4c1
Sahil-Prince <106463378+malangyar@users.noreply.github.com>
2022-06-29 18:47:12 +0500

    Update Malangdijan.py

--- Malangdijan.py
+++ Malangdijan.py
@@ -1,5 +1,7 @@
 import os
 os.system('git pull')
 os.system ('termux-setup-storage')
-print("\033m[1;32m  Welcome To My Tool")
+print("\033[1;32m  Welcome To My Tool")
+print("\033[1;31m  Teri Maa Ki Chut Ma Lun")
+print("\033[1;32m  Coded by Sahil")
 import Malang
